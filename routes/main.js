a51d86be7f79ac2091704794b5433c5e96aff985
coding-guy <1codingguy@gmail.com>
2022-09-05 21:18:36 +0100

    draft authentication middleware

--- routes/main.js
+++ routes/main.js
@@ -3,7 +3,10 @@ const router = express.Router()
 
 const { login, dashboard } = require('../controllers/main')
 
-router.route('/dashboard').get(dashboard)
+const authenticationMiddleware = require('../middleware/auth ')
+
+// chain authenticationMiddleware and dashboard controller function
+router.route('/dashboard').get(authenticationMiddleware, dashboard)
 router.route('/login').post(login)
 
 module.exports = router
