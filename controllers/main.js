f58b0f474417480648e36ab9202e80aa2cf48bb3
coding-guy <1codingguy@gmail.com>
2022-09-05 15:02:31 +0100

    throw error if username or password not valid

--- controllers/main.js
+++ controllers/main.js
@@ -1,6 +1,14 @@
+const CustomAPIError = require('../errors/custom-error')
+
 const login = async (req, res) => {
+  const { username, password } = req.body
+  console.log(username, password)
+  if (!username || !password) {
+    console.log('username or password missing')
+    throw new CustomAPIError('username or password not valid', 400)
+  }
   res.send('fake Login/Register/Signup')
-}
+} 
 
 const dashboard = async (req, res) => {
   const luckyNumber = Math.floor(Math.random() * 100)
