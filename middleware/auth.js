9e81b4396cecae2b52e0c32fd8425858d286a4e0
coding-guy <1codingguy@gmail.com>
2022-09-06 08:53:53 +0100

    refactor authentication middleware

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,6 +1,24 @@
+const jwt = require('jsonwebtoken')
+const CustomAPIError = require('../errors/custom-error')
+
 const authenticationMiddleware = async (req, res, next) => {
-  console.log(req.headers.authorization)
-  next()
+  // check if there's `authorization` property in req.headers
+  const { authorization: authHeader } = req.headers
+
+  if (!authHeader || !authHeader.startsWith('Bearer ')) {
+    throw new CustomAPIError('No token provided', 401)
+  }
+
+  const token = authHeader.split(' ')[1]
+
+  try {
+    const decoded = jwt.verify(token, process.env.JWT_STRING)
+    const { id, username } = decoded
+    req.user = { id, username }
+    next()
+  } catch (error) {
+    throw new CustomAPIError('No authorized to access this route ', 401)
+  }
 }
 
 module.exports = authenticationMiddleware
--- routes/main.js
+++ routes/main.js
@@ -3,7 +3,7 @@ const router = express.Router()
 
 const { login, dashboard } = require('../controllers/main')
 
-const authenticationMiddleware = require('../middleware/auth ')
+const authenticationMiddleware = require('../middleware/auth')
 
 // chain authenticationMiddleware and dashboard controller function
 router.route('/dashboard').get(authenticationMiddleware, dashboard)
